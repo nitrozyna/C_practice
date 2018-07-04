a10903cd6be45562e553da136e30b6ef747b1aba
nitrozyna <kurkiewicz.margaret@gmail.com>
2018-07-04 23:06:12 +0100

    Program that replaces each string of one or more blanks by a single blank.

---
+++ Chapter1/blanks.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**Program that replaces a string of one or more blanks by a single blank (accounts for tabs)**/
+
+int main(void){
+
+  int c;
+  int blank = 0;
+
+  printf("Input some characters and press Ctrl+D.\n");
+
+  while (( c = getchar()) != EOF)
+
+    if (c == ' ' || c == '\t') {
+
+      if (!blank) {
+        blank = 1;
+        putchar(' ');
+      }
+
+    } else {
+        blank = 0;
+        putchar(c);
+    }
+
+    return 0;
+
+}
